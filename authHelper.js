a1a1f448a35395c8d1cb1e1e4b846bc411c7b279
Patel Drashti <19drashti96@gmail.com>
2017-05-29 22:50:01 +0800

    Create a new file called authHelper.js. Initiate the OAuth2 Authorization Code using simple-oauth2 library --> "npm install simple-oauth2 --save"

---
+++ authHelper.js
@@ -0,0 +1,30 @@
+var credentials = {
+  client: {
+    id: '72c7d826-d6f3-4aaf-a3f5-6ba648d91661',
+    secret: '7cOYbDp7KWCgeoJWDjjUqF3',
+  },
+  auth: {
+    tokenHost: 'https://login.microsoftonline.com',
+    authorizePath: 'common/oauth2/v2.0/authorize',
+    tokenPath: 'common/oauth2/v2.0/token'
+  }
+};
+var oauth2 = require('simple-oauth2')(credentials);
+
+var redirectUri = 'http://localhost:8000/authorize';
+
+// The scopes the app requires
+var scopes = [ 'openid',
+               'User.Read',
+               'Mail.Read' ];
+
+function getAuthUrl() {
+  var returnVal = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: redirectUri,
+    scope: scopes.join(' ')
+  });
+  console.log('Generated auth url: ' + returnVal);
+  return returnVal;
+}
+
+exports.getAuthUrl = getAuthUrl;
