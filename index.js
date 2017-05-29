8dc1c7fdaea47a1bf0f626426076b793800e7226
Patel Drashti <19drashti96@gmail.com>
2017-05-29 23:00:00 +0800

    Updated handle array in .\index.js Add authorize function in index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var authHelper = require('./authHelper');
 
 var handle = {};
 handle['/'] = home;
+handle['/authorize'] = authorize;
 
 server.start(router.route, handle);
 
@@ -11,5 +12,18 @@ function home(response, request) {
   console.log('Request handler \'home\' was called.');
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.write('<p>Please <a href="' + authHelper.getAuthUrl() + '">sign in</a> with your Office 365 or Outlook.com account.</p>');
+  response.end();
+}
+
+var url = require('url');
+function authorize(response, request) {
+  console.log('Request handler \'authorize\' was called.');
+
+  // The authorization code is passed as a query parameter
+  var url_parts = url.parse(request.url, true);
+  var code = url_parts.query.code;
+  console.log('Code: ' + code);
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<p>Received auth code: ' + code + '</p>');
   response.end();
 }
